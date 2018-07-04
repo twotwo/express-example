c5a7dd58e5a7b98a4b26cea59bba6247d879bcfd
twotwo <twotwo.li@gmail.com>
2018-07-04 19:01:50 +0800

    用 jest 重写integration test

--- test/integration/user-creation.test.js
+++ test/integration/user-creation.test.js
@@ -1,39 +1,57 @@
-'use strict';
+"use strict"
 
-var app      = require('../../app');
-var Bluebird = require('bluebird');
-var expect   = require('expect.js');
-var request  = require('supertest');
+const app = require("../../app")
+const Bluebird = require("bluebird")
+const request = require("supertest")
 
-describe('user creation page', function () {
-  before(function () {
-      return require('../../models').sequelize.sync();
-  });
-  
-  beforeEach(function () {
-    this.models = require('../../models');
+const debug = require("debug")("test:integration")
+
+describe("user creation page", () => {
+  beforeAll(() => {
+    return require("../../models").sequelize.sync()
+  })
+
+  beforeEach(() => {
+    this.models = require("../../models")
 
     return Bluebird.all([
       this.models.Task.destroy({ truncate: true }),
       this.models.User.destroy({ truncate: true })
-    ]);
-  });
+    ])
+    // .then(results => {
+    //   debug("beforeEach clean data")
+    // })
+    // .catch(console.error)
+  })
 
-  it('loads correctly', function (done) {
-    request(app).get('/').expect(200, done);
-  });
+  test("loads correctly", done => {
+    request(app)
+      .get("/")
+      .expect(200, done)
+  })
 
-  it('lists a user if there is one', function (done) {
-    this.models.User.create({ username: 'johndoe' }).then(function () {
-      request(app).get('/').expect(/johndoe/, done);
+  test("lists a user if there is one", done => {
+    debug("User = %O", this.models.User)
+    this.models.User.create({ username: "johndoe" }).then(() => {
+      request(app)
+        .get("/")
+        .expect(/johndoe/, done)
     })
-  });
+  })
 
-  it('lists the tickets for the user if available', function (done) {
-    this.models.User.create({ username: 'johndoe' }).bind(this).then(function (user) {
-      return this.models.Task.create({ title: 'johndoe task', UserId: user.id });
-    }).then(function () {
-      request(app).get('/').expect(/johndoe task/, done);
-    });
-  });
-});
+  test("lists the tickets for the user if available", done => {
+    this.models.User.create({ username: "johndoe" })
+      .bind(this)
+      .then(function(user) {
+        return this.models.Task.create({
+          title: "johndoe task",
+          UserId: user.id
+        })
+      })
+      .then(() => {
+        request(app)
+          .get("/")
+          .expect(/johndoe task/, done)
+      })
+  })
+})
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -3,15 +3,18 @@
 const debug = require("debug")("test:unit:models/index")
 
 describe("models/index", () => {
+
+  beforeEach(() => {
+    this.models = require("../../models")
+  })
+
   test("returns the task model", () => {
-    const models = require("../../models")
-    // debug("models.Task = %O", models.Task)
-    expect(models.Task).not.toBeNull()
+    // debug("Task = %O", this.models.Task)
+    expect(this.models.Task).not.toBeNull()
   })
 
   test("returns the user model", () => {
-    const models = require("../../models")
-    // debug("models.User = %O", models.User)
-    expect(models.User).not.toBeNull()
+    // debug("User = %O", this.models.User)
+    expect(this.models.User).not.toBeNull()
   })
 })
