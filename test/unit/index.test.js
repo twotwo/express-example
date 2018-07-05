e171bfd6f3693603ecec5ff2f676e32d4384a8af
twotwo <twotwo.li@gmail.com>
2018-07-05 19:13:39 +0800

    单元测试用例优化

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -3,18 +3,17 @@
 const debug = require("debug")("test:unit:models/index")
 
 describe("models/index", () => {
-
   beforeEach(() => {
     this.models = require("../../models")
   })
 
   test("returns the task model", () => {
     // debug("Task = %O", this.models.Task)
-    expect(this.models.Task).not.toBeNull()
+    expect(this.models.Task.name).toBe("Task")
   })
 
   test("returns the user model", () => {
     // debug("User = %O", this.models.User)
-    expect(this.models.User).not.toBeNull()
+    expect(this.models.User.name).not.toBeNull()
   })
 })
