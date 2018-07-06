9747556bf599c6d1e272449fab5fef4fdc341d9a
twotwo <twotwo.li@gmail.com>
2018-07-06 18:19:11 +0800

    update Users table define

--- models/user.js
+++ models/user.js
@@ -1,12 +1,27 @@
-'use strict';
+"use strict"
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING
-  });
+  const User = sequelize.define(
+    "User",
+    {
+      username: { type: DataTypes.STRING(50), primaryKey: true },
+      password: { type: DataTypes.STRING(50), allowNull: false }
+    },
+    {
+      // http://docs.sequelizejs.com/manual/tutorial/models-definition.html#configuration
+      // Timestamps
+      // createdAt: DataTypes.DATE,
+      // updatedAt: DataTypes.DATE
+      // add the timestamp attributes (updatedAt, createdAt)
+      timestamps: true,
+      underscored: true,
+      freezeTableName: true,
+      tableName: "Users"
+    }
+  ) //使用下划线命名法 //不自动修改表名 // define the table's name
 
   User.associate = function(models) {
-    models.User.hasMany(models.Task);
-  };
+    models.User.hasMany(models.Task)
+  }
 
-  return User;
-};
+  return User
+}
